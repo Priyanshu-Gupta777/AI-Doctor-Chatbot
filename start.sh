c6f8c9c3082440328d9f09a7b426e14321cb84f6
Priyanshu-Gupta777 <priyanshugp777@gmail.com>
2025-03-24 18:10:38 +0530

    Remove some unnecessary files

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-# Install system dependencies
-apt-get update && apt-get install -y portaudio19-dev
-
-# Install Python dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
